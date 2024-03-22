c81e72ea748ca51dc56857e555da43b09e8d8f8a
Eric Huang <huanger@berkeley.edu>
2024-03-21 18:13:18 -0700

    removed sliding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,36 +27,43 @@ public class PlayerController : MonoBehaviour
 	void Update()
 	{
 		//Movement
+
 		float MoveHor = Input.GetAxisRaw("Horizontal");
-		Vector3 movement = new Vector3(MoveHor * movespeed, 0, 0);
-		movement = movement * Time.deltaTime;
+		if (MoveHor == 0) {
+			playerRB.velocity = new Vector3(0,playerRB.velocity.y, playerRB.velocity.z);
+		} else {
+			Vector3 movement = new Vector3(MoveHor * movespeed, 0, 0);
+			movement = movement * Time.deltaTime;
 
-		playerRB.AddForce(movement);
-		if (playerRB.velocity.x > maxspeed)
-		{
-			playerRB.velocity = new Vector3(maxspeed, 0, playerRB.velocity.z);
+			playerRB.AddForce(movement);
+			if (playerRB.velocity.x > maxspeed)
+			{
+				playerRB.velocity = new Vector3(maxspeed, 0, playerRB.velocity.z);
+			}
+			if (playerRB.velocity.x < -maxspeed)
+			{
+				playerRB.velocity = new Vector3(-maxspeed, 0, playerRB.velocity.z);
+			}
 		}
-		if (playerRB.velocity.x < -maxspeed)
-		{
-			playerRB.velocity = new Vector3(-maxspeed, 0, playerRB.velocity.z);
-		}
-
 		if (!has_gravity) {
 			float MoveVert = Input.GetAxisRaw("Vertical");
-			Vector3 movementy = new Vector3(0, 0, MoveVert * movespeed);
-			movementy = movementy * Time.deltaTime;
+			if (MoveVert == 0) {
+				playerRB.velocity = new Vector3(playerRB.velocity.x, playerRB.velocity.y, 0);
+			} else {
+				Vector3 movementy = new Vector3(0, 0, MoveVert * movespeed);
+				movementy = movementy * Time.deltaTime;
 
-			playerRB.AddForce(movementy);
-			if (playerRB.velocity.z > maxspeed)
-			{
-				playerRB.velocity = new Vector3(playerRB.velocity.x, 0, maxspeed);
-			}
-			if (playerRB.velocity.z < -maxspeed)
-			{
-				playerRB.velocity = new Vector3(playerRB.velocity.x, 0, -maxspeed);
-			}
+				playerRB.AddForce(movementy);
+				if (playerRB.velocity.z > maxspeed)
+				{
+					playerRB.velocity = new Vector3(playerRB.velocity.x, 0, maxspeed);
+				}
+				if (playerRB.velocity.z < -maxspeed)
+				{
+					playerRB.velocity = new Vector3(playerRB.velocity.x, 0, -maxspeed);
+				}
+			}	
 		}
-
 	}
 
 	#region GravityFunctions
