fb9ef4d6e096c08a258863b02c5bd61bf3f6686c
Lawrence Lam <lawrencelam@berkeley.edu>
2024-04-17 20:14:23 -0700

    added new levels to main menu and ui fixes

--- Assets/Scripts/LevelsManager.cs
+++ Assets/Scripts/LevelsManager.cs
@@ -33,5 +33,17 @@ public class LevelsManager : MonoBehaviour
     public void Level4() {
         SceneManager.LoadScene("Level4");
     }
+    public void Level5() {
+        SceneManager.LoadScene("Level5");
+    }
+    public void Level6() {
+        SceneManager.LoadScene("Level6");
+    }
+    public void Level7() {
+        SceneManager.LoadScene("Level7");
+    }
+    public void Level8() {
+        SceneManager.LoadScene("Level8");
+    }
     #endregion
 }
